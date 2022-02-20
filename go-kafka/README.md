d948382e35de448224e0c29ca9692394d7cb2936
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-02-20 16:56:33 -0300

    Kafka publish and delivery report sync

---
+++ go-kafka/README.md
@@ -0,0 +1,13 @@
+
+
+```
+docker exec -it gokafka bash
+go mod init github.com/lourivaldo/fc2-gokafka
+go run cmd/producer/main.go
+
+docker exec -it go-kafka_kafka_1 bash
+kafka-topics --create --bootstrap-server=localhost:9092 --topic=teste --partitions=3
+kafka-console-consumer --bootstrap-server=localhost:9092 --topic=teste
+
+go get github.com/confluentinc/confluent-kafka-go/kafka
+```
---
+++ go-kafka/cmd/producer/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"log"
+)
+
+func main() {
+	deliveryChan := make(chan kafka.Event)
+	producer := NewKafkaProducer()
+	Publish("mensagem", "teste", producer, nil, deliveryChan)
+
+	e := <-deliveryChan
+	msg := e.(*kafka.Message)
+	if msg.TopicPartition.Error != nil {
+		fmt.Println("Erro ao enviar")
+	} else {
+		fmt.Println("Mensagem enviada:", msg.TopicPartition)
+	}
+
+	producer.Flush(1000)
+}
+
+func NewKafkaProducer() *kafka.Producer {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "go-kafka_kafka_1:9092",
+	}
+	p, err := kafka.NewProducer(configMap)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return p
+}
+
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChan chan kafka.Event) error {
+	message := &kafka.Message{
+		Value:          []byte(msg),
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Key:            key,
+	}
+	err := producer.Produce(message, deliveryChan)
+	if err != nil {
+		return err
+	}
+	return nil
+}
